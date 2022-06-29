cf8697d9560cbde97f4874fa9ccf97c600968cc9
ULUGBEK <ulugbek@ULUGBEKs-iMac.local>
2022-06-29 19:28:46 +0600

    this is the Hotel webiste using Html, Css, Js, Swiper

---
+++ assets/js/script.js
@@ -0,0 +1,21 @@
+// slider
+var swiper = new Swiper('.swiper', {
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});
+
+var swiper = new Swiper('.roomSwiper', {
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  });
+
+// burger menu
+// const burgerCtr = document.querySelector('#burger-menu');
+// const menuCtr = document.querySelector('#mobile-menu');
+// burgerCtr.addEventListener('click', function () {
+//     this.classList.toggle('active');
+//     menuCtr.classList.toggle('show_mobile_menu');
+// });
